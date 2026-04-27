5d6ba36835be8ef39b5dccf88f1a3c1b05fc064e
Eshal <eshalnaeem2008@gmail.com>
2026-04-27 13:04:04 +0500

    Adding StudyRoom classes

--- src/GUI/AdditionalClasses/CreateLabel.java
+++ src/GUI/AdditionalClasses/CreateLabel.java
@@ -1,21 +1,5 @@
 package GUI.AdditionalClasses;
 
-import java.awt.*;
-import javax.swing.*;
-
-public class CreateLabel extends JLabel{
-
-    //default constructor
-    public CreateLabel(String title){
-        this.setLocation(0,0);
-        this.setSize(700,50);
-        this.setBackground(Color.GRAY);
-        this.setForeground(Color.BLACK); //text color
-        this.setFont(new Font("Bookman Old Style" , Font.BOLD, 27));
-        this.setHorizontalAlignment(JLabel.CENTER);
-        this.setVerticalAlignment(JLabel.CENTER);
-        this.setText(title);
-        this.setOpaque(true);
-    }
+public class CreateLabel {
 
 }
