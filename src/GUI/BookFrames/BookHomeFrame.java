b406ed4348fe421dae58f416e2fb1a5b14922bf8
albashzia <ahmedalbashzia@gmail.com>
2026-04-28 17:40:46 +0500

    Creating button objects in BooksHomeFrame class

--- src/GUI/BookFrames/BookHomeFrame.java
+++ src/GUI/BookFrames/BookHomeFrame.java
@@ -3,9 +3,19 @@ package GUI.BookFrames;
 import GUI.AdditionalClasses.CreateFrame;
 import GUI.AdditionalClasses.CreateLabel;
 
+import javax.swing.*;
+
 public class BookHomeFrame {
     CreateFrame bhFrame = new CreateFrame();
     CreateLabel label = new CreateLabel("Add a Book");
+    JButton button1 = new JButton();
+    JButton button2 = new JButton();
+    JButton button3 = new JButton();
+    JButton button4 = new JButton();
+    JButton button5 = new JButton();
+    JButton button6 = new JButton();
+    JButton button7 = new JButton();
+    JButton button8 = new JButton();
 
     public BookHomeFrame(){
 
