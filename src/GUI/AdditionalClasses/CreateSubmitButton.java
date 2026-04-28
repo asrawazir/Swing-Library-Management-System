b60f2272bbe140049a0e3c9f55450c159c436450
asrawazir <waziralamasra@gmail.com>
2026-04-28 17:05:18 +0500

    Created a CreateSubmitButton class and added a constructor which takes String as a parameter implements all the  custom methods

--- src/GUI/AdditionalClasses/CreateSubmitButton.java
+++ src/GUI/AdditionalClasses/CreateSubmitButton.java
@@ -1,4 +1,17 @@
 package GUI.AdditionalClasses;
 
-public class CreateSubmitButton {
+import java.awt.*;
+import javax.swing.*;
+
+public class CreateSubmitButton extends JButton{
+
+    public CreateSubmitButton(String title){
+        this.setLocation(50,390);
+        this.setSize(100,40);
+        this.setFont(new Font("Roboto", Font.BOLD, 12));
+        this.setBackground(Color.LIGHT_GRAY);
+        this.setForeground(Color.BLACK);
+        this.setText(title);
+        this.setOpaque(true);
+    }
 }
