c605a7e2ca34a43d058862cf0597ca67304b73c3
Eshal <eshalnaeem2008@gmail.com>
2026-04-28 23:47:25 +0500

    Created a StudyRoomHomeFrame class with initial frame components .

--- src/GUI/StudyRoomFrames/StudyRoomHomeFrame.java
+++ src/GUI/StudyRoomFrames/StudyRoomHomeFrame.java
@@ -1,4 +1,56 @@
 package GUI.StudyRoomFrames;
 
-public class StudyRoomHomeFrame {
-}
+import GUI.AdditionalClasses.CreateBackButton;
+import GUI.AdditionalClasses.CreateFrame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class StudyRoomHomeFrame  {
+       CreateFrame frame;
+       CreateBackButton button;
+       JButton button1=new JButton();
+       JButton button2=new JButton();
+       JButton button3=new JButton();
+       JLabel l1=new JLabel("STUDY ROOM MANAGEMENT");
+
+
+   public  StudyRoomHomeFrame(){
+       frame =new CreateFrame("StudyRoom Frame");
+       button=new CreateBackButton();
+
+        l1.setBounds(150,30,400,40);
+        l1.setFont(new Font("Bookman Old Style",Font.BOLD,25));
+        l1.setHorizontalAlignment(SwingConstants.CENTER);
+
+
+        button1.setBounds(200,100,250,40);
+        button2.setBounds(200,170,250,40);
+        button3.setBounds(200,240,250,40);
+
+
+        button1.setText("Display Rooms");
+        button2.setText("Reserve Rooms");
+        button3.setText("Cancel Reservation");
+
+        button1.setFocusable(false);
+        button2.setFocusable(false);
+        button3.setFocusable(false);
+
+
+
+
+        frame.add(l1);
+        frame.add(button1);
+        frame.add(button2);
+        frame.add(button3);
+        frame.add(button);
+
+
+
+       frame.setVisible(true);
+
+   }
+       }
