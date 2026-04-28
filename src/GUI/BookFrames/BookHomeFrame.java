47c1b62acc41446d5d96701f036c91e3e77fca66
albashzia <ahmedalbashzia@gmail.com>
2026-04-28 17:30:23 +0500

    Creating a CreateFrame and CreateLabel object in BookHomeFrame class

--- src/GUI/BookFrames/BookHomeFrame.java
+++ src/GUI/BookFrames/BookHomeFrame.java
@@ -1,4 +1,15 @@
 package GUI.BookFrames;
 
+import GUI.AdditionalClasses.CreateFrame;
+import GUI.AdditionalClasses.CreateLabel;
+
 public class BookHomeFrame {
+    CreateFrame bhFrame = new CreateFrame();
+    CreateLabel label = new CreateLabel("Add a Book");
+
+    public BookHomeFrame(){
+
+        bhFrame.add(label);
+        bhFrame.setVisible(true);
+    }
 }
