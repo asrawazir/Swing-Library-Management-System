b1a3e3147e647dc44cbccabd920098c1fe39d6ba
asrawazir <waziralamasra@gmail.com>
2026-04-28 16:35:53 +0500

    Created a CreateLabel class and added a constructor which takes string as a parameter and implements all the  custom functions

--- src/GUI/AdditionalClasses/CreateLabel.java
+++ src/GUI/AdditionalClasses/CreateLabel.java
@@ -10,7 +10,7 @@ public class CreateLabel extends JLabel{
         this.setLocation(0,0);
         this.setSize(700,50);
         this.setBackground(Color.DARK_GRAY);
-        this.setForeground(Color.BLACK);
+        this.setForeground(Color.BLACK); //text color
         this.setFont(new Font("Bookman Old Style" , Font.BOLD, 27));
         this.setHorizontalAlignment(JLabel.CENTER);
         this.setVerticalAlignment(JLabel.CENTER);
