8c7234687d4ab9e587f123d032b09fe7bcfd0e90
albashzia <ahmedalbashzia@gmail.com>
2026-04-28 17:42:55 +0500

    Defining the text for JButton objects in BookHomeFrame class

--- src/GUI/BookFrames/BookHomeFrame.java
+++ src/GUI/BookFrames/BookHomeFrame.java
@@ -8,14 +8,14 @@ import javax.swing.*;
 public class BookHomeFrame {
     CreateFrame bhFrame = new CreateFrame();
     CreateLabel label = new CreateLabel("Add a Book");
-    JButton button1 = new JButton();
-    JButton button2 = new JButton();
-    JButton button3 = new JButton();
-    JButton button4 = new JButton();
-    JButton button5 = new JButton();
-    JButton button6 = new JButton();
-    JButton button7 = new JButton();
-    JButton button8 = new JButton();
+    JButton button1 = new JButton("Add Book");
+    JButton button2 = new JButton("Remove Book");
+    JButton button3 = new JButton("Search Book by ID");
+    JButton button4 = new JButton("Search Book by Author");
+    JButton button5 = new JButton("Display Available Books");
+    JButton button6 = new JButton("Issue Book");
+    JButton button7 = new JButton("Return Book");
+    JButton button8 = new JButton("Show Issued Books");
 
     public BookHomeFrame(){
 
