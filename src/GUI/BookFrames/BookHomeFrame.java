b4c17cbfe1fa46eada51ec0e10726470bd3dace6
albashzia <ahmedalbashzia@gmail.com>
2026-04-29 12:56:57 +0500

    Creating FrameButtons object in BookHomeFrame class passing title and x,y coordinates

--- src/GUI/BookFrames/BookHomeFrame.java
+++ src/GUI/BookFrames/BookHomeFrame.java
@@ -2,19 +2,20 @@ package GUI.BookFrames;
 
 import GUI.AdditionalClasses.CreateFrame;
 import GUI.AdditionalClasses.CreateLabel;
+import GUI.AdditionalClasses.FrameButtons;
 
 import javax.swing.*;
 
 public class BookHomeFrame {
     CreateFrame bhFrame = new CreateFrame();
     CreateLabel label = new CreateLabel("Books Management System");
-    JButton button1 = new JButton("Add Book");
-    JButton button2 = new JButton("Remove Book");
-    JButton button3 = new JButton("Search Book");
-    JButton button5 = new JButton("Display Available Books");
-    JButton button6 = new JButton("Issue Book");
-    JButton button7 = new JButton("Return Book");
-    JButton button8 = new JButton("Show Issued Books");
+    FrameButtons button1 = new FrameButtons("Add Book",20,20);
+    FrameButtons button2 = new FrameButtons("Remove Book",30,30);
+    FrameButtons button3 = new FrameButtons("Search Book",40,40);
+    FrameButtons button5 = new FrameButtons("Display Available Books",50,50);
+    FrameButtons button6 = new FrameButtons("Issue Book",60,60);
+    FrameButtons button7 = new FrameButtons("Return Book",70,70);
+    FrameButtons button8 = new FrameButtons("Show Issued Books",80,80);
 
     public BookHomeFrame(){
 
