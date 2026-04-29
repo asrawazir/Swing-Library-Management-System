e6f1758eb20fbd4a6109c9e9e1d893b16320f747
asrawazir <waziralamasra@gmail.com>
2026-04-29 13:29:12 +0500

    Modifying the coordinates of the buttons

--- src/GUI/AdditionalClasses/CreateBackButton.java
+++ src/GUI/AdditionalClasses/CreateBackButton.java
@@ -7,12 +7,13 @@ public class CreateBackButton extends JButton{
 
     //default constructor
     public CreateBackButton(){
-        this.setLocation(480,390);
+        this.setLocation(50,390);
         this.setSize(80,40);
         this.setFont(new Font("Roboto", Font.BOLD, 12));
         this.setBackground(Color.LIGHT_GRAY);
         this.setForeground(Color.BLACK);
         this.setText("Back");
         this.setOpaque(true);
+        this.setFocusable(false);
     }
 }
