943190706576c6fa6128f1b8c2c82d2749f0738d
albashzia <ahmedalbashzia@gmail.com>
2026-04-29 17:08:53 +0500

    Creating an back button using CreateBackButton class and adding it to the frame for removing the book from the LMS

--- src/GUI/BookFrames/RemoveBook.java
+++ src/GUI/BookFrames/RemoveBook.java
@@ -1,5 +1,6 @@
 package GUI.BookFrames;
 
+import GUI.AdditionalClasses.CreateBackButton;
 import GUI.AdditionalClasses.CreateExitButton;
 import GUI.AdditionalClasses.CreateFrame;
 import GUI.AdditionalClasses.CreateLabel;
@@ -8,8 +9,11 @@ public class RemoveBook {
     CreateFrame rbFrame = new CreateFrame();
     CreateLabel label = new CreateLabel("Remove Book");
     CreateExitButton exitButton = new CreateExitButton();
+    CreateBackButton backButton = new CreateBackButton();
+
     public RemoveBook(){
 
+        rbFrame.add(backButton);
         rbFrame.add(exitButton);
         rbFrame.add(label);
         rbFrame.setVisible(true);
