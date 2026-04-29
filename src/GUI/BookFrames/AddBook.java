0066affabbcf418a255df73642de34660d328706
albashzia <ahmedalbashzia@gmail.com>
2026-04-29 15:27:24 +0500

    Creating a basic frame in AddBook class using CreateBookClass

--- src/GUI/BookFrames/AddBook.java
+++ src/GUI/BookFrames/AddBook.java
@@ -1,9 +1,13 @@
 package GUI.BookFrames;
 
+import GUI.AdditionalClasses.CreateFrame;
+
 import javax.swing.*;
 
 public class AddBook {
+    CreateFrame abFrame = new CreateFrame();
     public AddBook(){
 
+        abFrame.setVisible(true);
     }
 }
