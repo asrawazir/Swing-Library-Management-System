650e9adaf45d3b97d8a3bb1d6201de1d7e41ca24
albashzia <ahmedalbashzia@gmail.com>
2026-04-29 15:35:08 +0500

    Creating a CreateLabel object in AddBook frame and adding it to the frame of AddBook class

--- src/GUI/BookFrames/AddBook.java
+++ src/GUI/BookFrames/AddBook.java
@@ -1,13 +1,16 @@
 package GUI.BookFrames;
 
 import GUI.AdditionalClasses.CreateFrame;
+import GUI.AdditionalClasses.CreateLabel;
 
 import javax.swing.*;
 
 public class AddBook {
     CreateFrame abFrame = new CreateFrame();
+    CreateLabel label = new CreateLabel("Add a Book");
     public AddBook(){
 
+        abFrame.add(label);
         abFrame.setVisible(true);
     }
 }
