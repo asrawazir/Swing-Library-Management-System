a854655cb4b0d903a352c26fab4533ceeabb60f1
albashzia <ahmedalbashzia@gmail.com>
2026-04-29 13:11:11 +0500

    Adjusting the x and y coordinates of the buttons and adding them to BookHomeFrame

--- src/GUI/BookFrames/BookHomeFrame.java
+++ src/GUI/BookFrames/BookHomeFrame.java
@@ -9,16 +9,23 @@ import javax.swing.*;
 public class BookHomeFrame {
     CreateFrame bhFrame = new CreateFrame();
     CreateLabel label = new CreateLabel("Books Management System");
-    FrameButtons button1 = new FrameButtons("Add Book",20,20);
-    FrameButtons button2 = new FrameButtons("Remove Book",30,30);
-    FrameButtons button3 = new FrameButtons("Search Book",40,40);
-    FrameButtons button5 = new FrameButtons("Display Available Books",50,50);
-    FrameButtons button6 = new FrameButtons("Issue Book",60,60);
-    FrameButtons button7 = new FrameButtons("Return Book",70,70);
-    FrameButtons button8 = new FrameButtons("Show Issued Books",80,80);
+    FrameButtons button1 = new FrameButtons("Add Book",60,100);
+    FrameButtons button2 = new FrameButtons("Remove Book",260,100);
+    FrameButtons button3 = new FrameButtons("Search Book",460,100);
+    FrameButtons button4 = new FrameButtons("Display Books",60,200);
+    FrameButtons button5 = new FrameButtons("Issue Book",260,200);
+    FrameButtons button6 = new FrameButtons("Return Book",460,200);
+    FrameButtons button7 = new FrameButtons("Show Issued Books",260,300);
 
     public BookHomeFrame(){
 
+        bhFrame.add(button1);
+        bhFrame.add(button2);
+        bhFrame.add(button3);
+        bhFrame.add(button4);
+        bhFrame.add(button5);
+        bhFrame.add(button6);
+        bhFrame.add(button7);
         bhFrame.add(label);
         bhFrame.setVisible(true);
     }
