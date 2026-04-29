e8d5421dc8fa91c84a3360e36a4aff12a5ba1e4e
albashzia <ahmedalbashzia@gmail.com>
2026-04-29 15:37:33 +0500

    Creating JLabel objects in AddBook class

--- src/GUI/BookFrames/AddBook.java
+++ src/GUI/BookFrames/AddBook.java
@@ -8,6 +8,12 @@ import javax.swing.*;
 public class AddBook {
     CreateFrame abFrame = new CreateFrame();
     CreateLabel label = new CreateLabel("Add a Book");
+
+    JLabel idLabel = new JLabel();
+    JLabel titleLabel = new JLabel();
+    JLabel authorLabel = new JLabel();
+    JLabel quantityLabel = new JLabel();
+
     public AddBook(){
 
         abFrame.add(label);
