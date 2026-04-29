483d513508e9ef97e276a66f135d4f7cb7cc5cdd
albashzia <ahmedalbashzia@gmail.com>
2026-04-29 17:02:12 +0500

    Defining a constructor in RemoveBook class and making the frame visible using setVisible method

--- src/GUI/BookFrames/RemoveBook.java
+++ src/GUI/BookFrames/RemoveBook.java
@@ -1,4 +1,12 @@
 package GUI.BookFrames;
 
+import GUI.AdditionalClasses.CreateFrame;
+
 public class RemoveBook {
+    CreateFrame rbFrame = new CreateFrame();
+
+    public RemoveBook(){
+
+        rbFrame.setVisible(true);
+    }
 }
