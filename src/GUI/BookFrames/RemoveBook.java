51d6e0e491e699f3e0d5b923d6a34d6287c7dfd8
albashzia <ahmedalbashzia@gmail.com>
2026-04-29 17:07:37 +0500

    Creating an exit button using CreateExitButton class and adding it to the object of RemoveBook class

--- src/GUI/BookFrames/RemoveBook.java
+++ src/GUI/BookFrames/RemoveBook.java
@@ -1,13 +1,16 @@
 package GUI.BookFrames;
 
+import GUI.AdditionalClasses.CreateExitButton;
 import GUI.AdditionalClasses.CreateFrame;
 import GUI.AdditionalClasses.CreateLabel;
 
 public class RemoveBook {
     CreateFrame rbFrame = new CreateFrame();
     CreateLabel label = new CreateLabel("Remove Book");
+    CreateExitButton exitButton = new CreateExitButton();
     public RemoveBook(){
 
+        rbFrame.add(exitButton);
         rbFrame.add(label);
         rbFrame.setVisible(true);
     }
