dd5034cd19d543401a73b966897ff938e0ecf413
albashzia <ahmedalbashzia@gmail.com>
2026-04-29 17:06:15 +0500

    Creating a CreateLabel object and adding it to the object of RemoveBook class

--- src/GUI/BookFrames/RemoveBook.java
+++ src/GUI/BookFrames/RemoveBook.java
@@ -1,12 +1,14 @@
 package GUI.BookFrames;
 
 import GUI.AdditionalClasses.CreateFrame;
+import GUI.AdditionalClasses.CreateLabel;
 
 public class RemoveBook {
     CreateFrame rbFrame = new CreateFrame();
-
+    CreateLabel label = new CreateLabel("Remove Book");
     public RemoveBook(){
 
+        rbFrame.add(label);
         rbFrame.setVisible(true);
     }
 }
