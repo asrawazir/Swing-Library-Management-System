85ab310f9e01e6bd1fbd0984dbac24d3dda854c3
albashzia <ahmedalbashzia@gmail.com>
2026-04-30 14:31:59 +0500

    Defining the constructor for IssueBook class, creating a frame using CreateFrame class and making it visible

--- src/GUI/BookFrames/IssueBook.java
+++ src/GUI/BookFrames/IssueBook.java
@@ -1,4 +1,11 @@
 package GUI.BookFrames;
 
+import GUI.AdditionalClasses.CreateFrame;
+
 public class IssueBook {
+    CreateFrame ibFrame = new CreateFrame();
+    public IssueBook(){
+
+        ibFrame.setVisible(true);
+    }
 }
