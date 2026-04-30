0b7cf44c41c712ca2a7f2a17024c0e08f33435fc
albashzia <ahmedalbashzia@gmail.com>
2026-04-30 13:06:39 +0500

    Creating a CreateLabel object in DisplayBooks class and adding it to the frame.

--- src/GUI/BookFrames/DisplayBooks.java
+++ src/GUI/BookFrames/DisplayBooks.java
@@ -1,12 +1,15 @@
 package GUI.BookFrames;
 
 import GUI.AdditionalClasses.CreateFrame;
+import GUI.AdditionalClasses.CreateLabel;
 
 public class DisplayBooks {
     CreateFrame dbFrame = new CreateFrame();
+    CreateLabel label = new CreateLabel("Display Books");
 
     public DisplayBooks(){
 
+        dbFrame.add(label);
         dbFrame.setVisible(true);
     }
 }
