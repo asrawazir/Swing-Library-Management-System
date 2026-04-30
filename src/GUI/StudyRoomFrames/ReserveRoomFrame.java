0f91cc6f6efd0d547bb33aa856cda3768d8f3ca2
Eshal <eshalnaeem2008@gmail.com>
2026-04-30 19:21:37 +0500

    Add reserve room frame with member id and room input fields

--- src/GUI/StudyRoomFrames/ReserveRoomFrame.java
+++ src/GUI/StudyRoomFrames/ReserveRoomFrame.java
@@ -1,4 +1,78 @@
 package GUI.StudyRoomFrames;
 
-public class ReserveRoomFrame {
+import GUI.AdditionalClasses.CreateBackButton;
+import GUI.AdditionalClasses.CreateExitButton;
+import GUI.AdditionalClasses.CreateFrame;
+import GUI.AdditionalClasses.CreateLabel;
+import GUI.HomeFrame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ReserveRoomFrame implements ActionListener {
+    CreateFrame frame;
+    CreateBackButton backButton;
+    CreateLabel label;
+    CreateExitButton exitButton;
+    JLabel roomLabel = new JLabel();
+    JLabel memberIDLabel = new JLabel();
+    JTextField roomField = new JTextField();
+    JTextField memberField = new JTextField();
+
+    public ReserveRoomFrame() {
+        frame = new CreateFrame();
+        backButton = new CreateBackButton();
+        label = new CreateLabel("Reserve Room");
+        exitButton = new CreateExitButton();
+
+        backButton.addActionListener(this);
+        exitButton.addActionListener(this);
+
+        roomLabel.setText("Enter Room Number:");
+        memberIDLabel.setText("Enter Member ID:");
+
+        roomLabel.setBounds(10, 120, 680, 30);
+        memberIDLabel.setBounds(10, 190, 680, 30);
+
+        roomLabel.setFont(new Font("Inter", Font.BOLD, 14));
+        roomLabel.setBackground(Color.lightGray);
+
+        memberIDLabel.setFont(new Font("Inter", Font.BOLD, 14));
+        memberIDLabel.setBackground(Color.lightGray);
+
+        roomField.setBounds(10, 150, 670, 30);
+        memberField.setBounds(10, 220, 670, 30);
+
+        roomField.setFont(new Font("Inter", Font.PLAIN, 14));
+        memberField.setFont(new Font("Inter", Font.PLAIN, 14));
+
+        frame.add(label);
+        frame.add(roomLabel);
+        frame.add(roomField);
+        frame.add(memberIDLabel);
+        frame.add(memberField);
+        frame.add(backButton);
+        frame.add(exitButton);
+
+        frame.setVisible(true);
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == exitButton) {
+            frame.dispose();
+            new HomeFrame();
+
+        }
+        if (e.getSource() == backButton) {
+            frame.dispose();
+            new StudyRoomHomeFrame();
+        }
+
+
+    }
 }
