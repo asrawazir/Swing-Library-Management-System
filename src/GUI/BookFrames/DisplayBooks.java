2d86f5f5dd15b7ec466a5290adb81278d13ae3a2
albashzia <ahmedalbashzia@gmail.com>
2026-04-30 13:08:18 +0500

    Creating a back button and exit button object and adding them to DisplayBooks frame

--- src/GUI/BookFrames/DisplayBooks.java
+++ src/GUI/BookFrames/DisplayBooks.java
@@ -1,14 +1,20 @@
 package GUI.BookFrames;
 
+import GUI.AdditionalClasses.CreateBackButton;
+import GUI.AdditionalClasses.CreateExitButton;
 import GUI.AdditionalClasses.CreateFrame;
 import GUI.AdditionalClasses.CreateLabel;
 
 public class DisplayBooks {
     CreateFrame dbFrame = new CreateFrame();
     CreateLabel label = new CreateLabel("Display Books");
+    CreateBackButton backButton = new CreateBackButton();
+    CreateExitButton exitButton = new CreateExitButton();
 
     public DisplayBooks(){
 
+        dbFrame.add(backButton);
+        dbFrame.add(exitButton);
         dbFrame.add(label);
         dbFrame.setVisible(true);
     }
