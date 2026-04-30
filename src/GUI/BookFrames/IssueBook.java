7add4d68f85be553a7832c6a58a4957992cef17d
albashzia <ahmedalbashzia@gmail.com>
2026-04-30 15:46:55 +0500

    Creating JLabel and JTextField objects in IssueBook class

--- src/GUI/BookFrames/IssueBook.java
+++ src/GUI/BookFrames/IssueBook.java
@@ -3,9 +3,18 @@ package GUI.BookFrames;
 import GUI.AdditionalClasses.CreateFrame;
 import GUI.AdditionalClasses.CreateLabel;
 
+import javax.swing.*;
+
 public class IssueBook {
     CreateFrame ibFrame = new CreateFrame();
     CreateLabel label = new CreateLabel("Issue a Book");
+
+    JLabel memberIdLabel = new JLabel();
+    JLabel bookIdLabel = new JLabel();
+
+    JTextField memberIdTextField = new JTextField();
+    JTextField bookIdTextField = new JTextField();
+
     public IssueBook(){
 
         ibFrame.add(label);
