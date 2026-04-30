065e192ff61927bc8b49159cac234f54d7f0eeb0
albashzia <ahmedalbashzia@gmail.com>
2026-04-30 12:22:17 +0500

    Creating a CreateLabel object in SearchBookByID class and adding it to the frame.

--- src/GUI/BookFrames/SearchBookByID.java
+++ src/GUI/BookFrames/SearchBookByID.java
@@ -3,6 +3,7 @@ package GUI.BookFrames;
 import GUI.AdditionalClasses.CreateBackButton;
 import GUI.AdditionalClasses.CreateExitButton;
 import GUI.AdditionalClasses.CreateFrame;
+import GUI.AdditionalClasses.CreateLabel;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,11 +12,13 @@ public class SearchBookByID implements ActionListener {
     CreateFrame sbiFrame = new CreateFrame();
     CreateBackButton backButton = new CreateBackButton();
     CreateExitButton exitButton = new CreateExitButton();
+    CreateLabel label = new CreateLabel("Search Book by ID");
 
     public SearchBookByID(){
 
         backButton.addActionListener(this);
 
+        sbiFrame.add(label);
         sbiFrame.add(backButton);
         sbiFrame.add(exitButton);
         sbiFrame.setVisible(true);
