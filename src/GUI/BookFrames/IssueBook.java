b342cc7410dae8faa303f59add58c4a04fe5b9e6
albashzia <ahmedalbashzia@gmail.com>
2026-04-30 14:34:35 +0500

    Creating a CreateLabel object in IssueBook class and adding it to the frame.

--- src/GUI/BookFrames/IssueBook.java
+++ src/GUI/BookFrames/IssueBook.java
@@ -1,11 +1,14 @@
 package GUI.BookFrames;
 
 import GUI.AdditionalClasses.CreateFrame;
+import GUI.AdditionalClasses.CreateLabel;
 
 public class IssueBook {
     CreateFrame ibFrame = new CreateFrame();
+    CreateLabel label = new CreateLabel("Issue a Book");
     public IssueBook(){
 
+        ibFrame.add(label);
         ibFrame.setVisible(true);
     }
 }
