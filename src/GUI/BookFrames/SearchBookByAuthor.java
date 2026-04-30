e8f34093f09097f9338002d7f3b8f3225d5b6641
albashzia <ahmedalbashzia@gmail.com>
2026-04-30 10:35:10 +0500

    Creating and adding back button and exit buttons to SearchBookByAuthor frame

--- src/GUI/BookFrames/SearchBookByAuthor.java
+++ src/GUI/BookFrames/SearchBookByAuthor.java
@@ -1,12 +1,18 @@
 package GUI.BookFrames;
 
+import GUI.AdditionalClasses.CreateBackButton;
+import GUI.AdditionalClasses.CreateExitButton;
 import GUI.AdditionalClasses.CreateFrame;
 
 public class SearchBookByAuthor {
     CreateFrame sbaFrame = new CreateFrame();
+    CreateBackButton backButton = new CreateBackButton();
+    CreateExitButton exitButton = new CreateExitButton();
 
     public SearchBookByAuthor(){
 
+        sbaFrame.add(backButton);
+        sbaFrame.add(exitButton);
         sbaFrame.setVisible(true);
     }
 }
