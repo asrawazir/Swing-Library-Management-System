41548134e66edc27f4b884a96c222b3dbf51beec
albashzia <ahmedalbashzia@gmail.com>
2026-04-30 10:08:06 +0500

    Creating a frame for SearchBook class using CreateFrame class and making it visible

--- src/GUI/BookFrames/SearchBook.java
+++ src/GUI/BookFrames/SearchBook.java
@@ -1,8 +1,13 @@
 package GUI.BookFrames;
 
+import GUI.AdditionalClasses.CreateFrame;
+
 public class SearchBook {
+
+    CreateFrame sbFrame = new CreateFrame();
 
     public SearchBook(){
 
+        sbFrame.setVisible(true);
     }
 }
