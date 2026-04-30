db4350aa775b5e9faeea86099e02da9ff11eca57
albashzia <ahmedalbashzia@gmail.com>
2026-04-30 10:11:14 +0500

    Creating a CreateLabel object in SearchBook class, passing a string title to the constructor and adding it to the frame

--- src/GUI/BookFrames/SearchBook.java
+++ src/GUI/BookFrames/SearchBook.java
@@ -1,13 +1,15 @@
 package GUI.BookFrames;
 
 import GUI.AdditionalClasses.CreateFrame;
+import GUI.AdditionalClasses.CreateLabel;
 
 public class SearchBook {
 
     CreateFrame sbFrame = new CreateFrame();
-
+    CreateLabel label = new CreateLabel("Search Book");
     public SearchBook(){
 
+        sbFrame.add(label);
         sbFrame.setVisible(true);
     }
 }
