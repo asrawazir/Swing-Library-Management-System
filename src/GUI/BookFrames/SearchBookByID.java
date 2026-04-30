90b194e031a61168d88184fb62715546f67d0bbd
albashzia <ahmedalbashzia@gmail.com>
2026-04-30 10:20:57 +0500

    Defining the SearchBookByID constructor and making the frame visible

--- src/GUI/BookFrames/SearchBookByID.java
+++ src/GUI/BookFrames/SearchBookByID.java
@@ -1,4 +1,12 @@
 package GUI.BookFrames;
 
+import GUI.AdditionalClasses.CreateFrame;
+
 public class SearchBookByID {
+    CreateFrame sbiFrame = new CreateFrame();
+
+    public SearchBookByID(){
+
+        sbiFrame.setVisible(true);
+    }
 }
