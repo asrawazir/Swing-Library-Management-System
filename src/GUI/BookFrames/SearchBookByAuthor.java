8bbfdfa8f8b2bf0d18f377102fe684d2aa0d7940
albashzia <ahmedalbashzia@gmail.com>
2026-04-30 10:21:46 +0500

    Defining the SearchBookByAuthor constructor and making the frame visible

--- src/GUI/BookFrames/SearchBookByAuthor.java
+++ src/GUI/BookFrames/SearchBookByAuthor.java
@@ -1,4 +1,12 @@
 package GUI.BookFrames;
 
+import GUI.AdditionalClasses.CreateFrame;
+
 public class SearchBookByAuthor {
+    CreateFrame sbaFrame = new CreateFrame();
+
+    public SearchBookByAuthor(){
+
+        sbaFrame.setVisible(true);
+    }
 }
