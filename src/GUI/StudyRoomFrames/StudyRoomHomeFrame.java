02f67fd4cb7dc3133b8c7584bea045f8e356b171
Eshal <eshalnaeem2008@gmail.com>
2026-04-30 16:33:41 +0500

    Completing StudyRoomHomeFrame class

--- src/GUI/StudyRoomFrames/StudyRoomHomeFrame.java
+++ src/GUI/StudyRoomFrames/StudyRoomHomeFrame.java
@@ -2,55 +2,83 @@ package GUI.StudyRoomFrames;
 
 import GUI.AdditionalClasses.CreateBackButton;
 import GUI.AdditionalClasses.CreateFrame;
+import GUI.AdditionalClasses.CreateLabel;
+import GUI.AdditionalClasses.FrameButtons;
+import GUI.HomeFrame;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class StudyRoomHomeFrame  {
+public class StudyRoomHomeFrame implements ActionListener  {
        CreateFrame frame;
        CreateBackButton button;
-       JButton button1=new JButton();
-       JButton button2=new JButton();
-       JButton button3=new JButton();
-       JLabel l1=new JLabel("STUDY ROOM MANAGEMENT");
+       FrameButtons button1;
+       FrameButtons button2;
+       FrameButtons button3;
+       FrameButtons button4;
+       CreateLabel label;
 
 
    public  StudyRoomHomeFrame(){
        frame =new CreateFrame();
        button=new CreateBackButton();
+       label=new CreateLabel("StudyRoom Management ");
 
-        l1.setBounds(150,30,400,40);
-        l1.setFont(new Font("Bookman Old Style",Font.BOLD,25));
-        l1.setHorizontalAlignment(SwingConstants.CENTER);
+       button1=new FrameButtons("Display Rooms",100,150);
+       button2=new FrameButtons("Reserve Room",300,150);
+       button3=new FrameButtons("Cancel Reservation",100,220);
+       button4=new FrameButtons("Check Reservation Status",300,220);
 
+       button.addActionListener(this);
 
-        button1.setBounds(200,100,250,40);
-        button2.setBounds(200,170,250,40);
-        button3.setBounds(200,240,250,40);
+       button1.setFocusable(false);
+       button2.setFocusable(false);
+       button3.setFocusable(false);
+       button4.setFocusable(false);
 
+       button1.addActionListener(this);
+       button2.addActionListener(this);
+       button3.addActionListener(this);
+       button4.addActionListener(this);
 
-        button1.setText("Display Rooms");
-        button2.setText("Reserve Rooms");
-        button3.setText("Cancel Reservation");
-
-        button1.setFocusable(false);
-        button2.setFocusable(false);
-        button3.setFocusable(false);
-
-
-
-
-        frame.add(l1);
+        frame.add(label);
         frame.add(button1);
         frame.add(button2);
         frame.add(button3);
+        frame.add(button4);
         frame.add(button);
 
+        frame.setVisible(true);
 
+       }
 
-       frame.setVisible(true);
+    @Override
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource()==button1){
+            frame.dispose();
+            new DisplayAllRoomsFrame();
+
+        }
+        if(e.getSource()==button2){
+            frame.dispose();
+            new ReserveRoomFrame();
+        }
+        if(e.getSource()==button3){
+            frame.dispose();
+            new CancelReservationFrame();
+        }
+
+        if(e.getSource()==button4){
+            frame.dispose();
+            new CheckReservationFrame();
+
+        }
+        if(e.getSource()==button){
+            frame.dispose();
+            new HomeFrame();
+        }
 
    }
        }
