fecb27ba8f6f5e6afd2da061b3a46f26f88de43a
albashzia <ahmedalbashzia@gmail.com>
2026-04-30 12:56:57 +0500

    Defining the DisplayBooks constructor and making the frame visible

--- src/GUI/BookFrames/DisplayBooks.java
+++ src/GUI/BookFrames/DisplayBooks.java
@@ -1,4 +1,12 @@
 package GUI.BookFrames;
 
+import GUI.AdditionalClasses.CreateFrame;
+
 public class DisplayBooks {
+    CreateFrame dbFrame = new CreateFrame();
+
+    public DisplayBooks(){
+
+        dbFrame.setVisible(true);
+    }
 }
