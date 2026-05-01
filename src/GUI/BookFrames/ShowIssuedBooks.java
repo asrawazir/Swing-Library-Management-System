2a4090cfd61cd9ae924e9e239e361e2688db1d67
albashzia <ahmedalbashzia@gmail.com>
2026-05-01 05:48:24 +0500

    Using CreateLabel class to create a label by passing a String to its constructor and adding it to the ShowIssuedBooks frame

--- src/GUI/BookFrames/ShowIssuedBooks.java
+++ src/GUI/BookFrames/ShowIssuedBooks.java
@@ -1,11 +1,14 @@
 package GUI.BookFrames;
 
 import GUI.AdditionalClasses.CreateFrame;
+import GUI.AdditionalClasses.CreateLabel;
 
 public class ShowIssuedBooks {
     CreateFrame siFrame = new CreateFrame();
+    CreateLabel label = new CreateLabel("Issued Books");
     public ShowIssuedBooks(){
 
+        siFrame.add(label);
         siFrame.setVisible(true);
     }
 }
