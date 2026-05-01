9723f58e134045569babca867d0a269c191b52b9
albashzia <ahmedalbashzia@gmail.com>
2026-05-01 05:45:18 +0500

    Creating a ShowIssuedBooks constructor, instantiating a CreateFrame object and making it visible for the users

--- src/GUI/BookFrames/ShowIssuedBooks.java
+++ src/GUI/BookFrames/ShowIssuedBooks.java
@@ -1,4 +1,11 @@
 package GUI.BookFrames;
 
+import GUI.AdditionalClasses.CreateFrame;
+
 public class ShowIssuedBooks {
+    CreateFrame siFrame = new CreateFrame();
+    public ShowIssuedBooks(){
+
+        siFrame.setVisible(true);
+    }
 }
