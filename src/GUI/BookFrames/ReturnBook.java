da8934d0880febe1dd22ff5a06f217306d826e76
albashzia <ahmedalbashzia@gmail.com>
2026-05-01 05:28:31 +0500

    Defining the constructor for ReturnBook class, creating a frame using CreateFrame class and making it visible

--- src/GUI/BookFrames/ReturnBook.java
+++ src/GUI/BookFrames/ReturnBook.java
@@ -1,4 +1,11 @@
 package GUI.BookFrames;
 
+import GUI.AdditionalClasses.CreateFrame;
+
 public class ReturnBook {
+    CreateFrame rbFrame = new CreateFrame();
+    public ReturnBook(){
+
+        rbFrame.setVisible(true);
+    }
 }
