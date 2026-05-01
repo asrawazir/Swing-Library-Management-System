9c042616acf2b136ccc5dce91db9a7020c3bd728
Eshal <eshalnaeem2008@gmail.com>
2026-05-01 13:02:18 +0500

    Added member menu integration to HomeFrame

--- src/GUI/HomeFrame.java
+++ src/GUI/HomeFrame.java
@@ -1,6 +1,7 @@
 package GUI;
 import GUI.AdditionalClasses.*;
 import GUI.BookFrames.BookHomeFrame;
+import GUI.MemberFrames.MemberHomeFrame;
 import GUI.StudyRoomFrames.StudyRoomHomeFrame;
 
 import javax.swing.*;
@@ -15,15 +16,17 @@ public class HomeFrame implements ActionListener {
     FrameButtons button1;
     FrameButtons button2;
     FrameButtons button3;
+
     public HomeFrame(){
         frame=new CreateFrame();
-        label=new CreateLabel("HOME FRAME");
+        label=new CreateLabel("Home Frame");
         exitButton=new CreateExitButton();
         button1=new FrameButtons(" Book Menu",70,150);
         button2=new FrameButtons(" Member Menu",260,150);
         button3=new FrameButtons(" Room Menu",455,150);
 
         button1.addActionListener(this);
+        button2.addActionListener(this);
         button3.addActionListener(this);
 
         frame.add(label);
@@ -38,6 +41,11 @@ public class HomeFrame implements ActionListener {
         if (e.getSource()==button1){
             frame.dispose();
             new BookHomeFrame();
+        }
+        if(e.getSource()==button2){
+            frame.dispose();
+            new MemberHomeFrame();
+
         }
         if(e.getSource()==button3){
             frame.dispose();
