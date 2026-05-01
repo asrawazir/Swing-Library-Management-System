d39d18b77ec28d8e320f8b4de12b93c61767d2a2
asrawazir <waziralamasra@gmail.com>
2026-05-01 20:40:16 +0500

    Creating global instances for CreateFrame and CreateLabel class

--- src/GUI/MemberFrames/SearchMember.java
+++ src/GUI/MemberFrames/SearchMember.java
@@ -1,9 +1,16 @@
 package GUI.MemberFrames;
 
+import GUI.AdditionalClasses.CreateFrame;
+import GUI.AdditionalClasses.CreateLabel;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class SearchMember implements ActionListener {
+
+    CreateFrame smFrame;
+    CreateLabel smLabel;
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
