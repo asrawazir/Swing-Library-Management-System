bc1a03d924ddda07be056df5d944fedee28791bf
Eshal <eshalnaeem2008@gmail.com>
2026-05-01 17:59:27 +0500

    Added display all rooms frame with JTable integration

--- src/GUI/StudyRoomFrames/DisplayAllRoomsFrame.java
+++ src/GUI/StudyRoomFrames/DisplayAllRoomsFrame.java
@@ -1,4 +1,46 @@
 package GUI.StudyRoomFrames;
 
-public class DisplayAllRoomsFrame {
+import GUI.AdditionalClasses.*;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class DisplayAllRoomsFrame  {
+    CreateFrame frame;
+    CreateLabel label;
+    CreateBackButton backButton;
+
+    CreateExitButton exitButton;
+
+   public  DisplayAllRoomsFrame(){
+       frame=new CreateFrame();
+       label=new CreateLabel("Display All Rooms");
+       backButton=new CreateBackButton();
+      exitButton=new CreateExitButton();
+
+      String[] column={"Room Number","Status","Reserved By"};
+
+      String [][]data={
+              {"101","Available","null"},
+              {"102","Available","null"},
+              {"103","Available","null"},
+              {"104","Available","null"}
+      };
+
+      JTable table=new JTable(data,column);
+      JScrollPane scrollPane=new JScrollPane(table);
+
+      scrollPane.setBounds(100,150,500,90);
+      frame.add(label);
+      frame.add(backButton);
+      frame.add(exitButton);
+
+      frame.add(scrollPane);
+
+      frame.setVisible(true);
+
+
+
+
+    }
 }
