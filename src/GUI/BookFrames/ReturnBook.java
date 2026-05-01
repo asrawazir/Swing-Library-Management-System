dcf748e0723c00b9dd11639a815df3bed574e181
albashzia <ahmedalbashzia@gmail.com>
2026-05-01 05:29:51 +0500

    Creating a CreateLabel object in ReturnBook class and adding it to the frame

--- src/GUI/BookFrames/ReturnBook.java
+++ src/GUI/BookFrames/ReturnBook.java
@@ -1,11 +1,14 @@
 package GUI.BookFrames;
 
 import GUI.AdditionalClasses.CreateFrame;
+import GUI.AdditionalClasses.CreateLabel;
 
 public class ReturnBook {
     CreateFrame rbFrame = new CreateFrame();
+    CreateLabel label = new CreateLabel("Return Book");
     public ReturnBook(){
 
+        rbFrame.add(label);
         rbFrame.setVisible(true);
     }
 }
