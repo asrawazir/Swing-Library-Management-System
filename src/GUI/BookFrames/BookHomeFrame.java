a0dffae258003fe30202b3c12d0045a26acb4a18
albashzia <ahmedalbashzia@gmail.com>
2026-05-01 06:03:51 +0500

    Aligning the code as per the appearance sequence on the frame and commenting to make the code readable and easy to understand

--- src/GUI/BookFrames/BookHomeFrame.java
+++ src/GUI/BookFrames/BookHomeFrame.java
@@ -8,12 +8,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class BookHomeFrame implements ActionListener {
+
+    //Frame Creation
     CreateFrame bhFrame = new CreateFrame();
+
+    //Label Creation
     CreateLabel label = new CreateLabel("Books Management System");
 
-    CreateBackButton backButton = new CreateBackButton();
-    CreateExitButton exitButton = new CreateExitButton();
-
+    //Buttons defined in FrameButtons for options on the frame
     FrameButtons button1 = new FrameButtons("Add Book",60,100);
     FrameButtons button2 = new FrameButtons("Remove Book",260,100);
     FrameButtons button3 = new FrameButtons("Search Book",460,100);
@@ -22,13 +24,14 @@ public class BookHomeFrame implements ActionListener {
     FrameButtons button6 = new FrameButtons("Return Book",460,200);
     FrameButtons button7 = new FrameButtons("Show Issued Books",260,300);
 
+    //Pre-defined buttons for exit and back functions
+    CreateBackButton backButton = new CreateBackButton();
+    CreateExitButton exitButton = new CreateExitButton();
+
+    //Constructor Definition
     public BookHomeFrame(){
 
-        bhFrame.add(backButton);
-        bhFrame.add(exitButton);
-
-        backButton.addActionListener(this);
-
+        //adding action listener to all the defined buttons
         button1.addActionListener(this);
         button2.addActionListener(this);
         button3.addActionListener(this);
@@ -36,7 +39,12 @@ public class BookHomeFrame implements ActionListener {
         button5.addActionListener(this);
         button6.addActionListener(this);
         button7.addActionListener(this);
+        backButton.addActionListener(this);
 
+        //adding label to the frame
+        bhFrame.add(label);
+
+        //adding buttons to the frame
         bhFrame.add(button1);
         bhFrame.add(button2);
         bhFrame.add(button3);
@@ -44,15 +52,24 @@ public class BookHomeFrame implements ActionListener {
         bhFrame.add(button5);
         bhFrame.add(button6);
         bhFrame.add(button7);
-        bhFrame.add(label);
+        bhFrame.add(backButton);
+        bhFrame.add(exitButton);
+
+        //making the frame visible
         bhFrame.setVisible(true);
     }
 
+    //Overriding the actionPerformed method defined in ActionListener interface
+    @Override
     public void actionPerformed(ActionEvent e){
+
+        //functionality for back button
         if (e.getSource()==backButton){
             bhFrame.dispose();
             new HomeFrame();
         }
+
+        //Functionality for other frame buttons
         if (e.getSource()==button1){
             bhFrame.dispose();
             new AddBook();
@@ -61,27 +78,22 @@ public class BookHomeFrame implements ActionListener {
             bhFrame.dispose();
             new RemoveBook();
         }
-
         if (e.getSource()==button3){
             bhFrame.dispose();
             new SearchBook();
         }
-
         if (e.getSource()==button4){
             bhFrame.dispose();;
             new DisplayBooks();
         }
-
         if (e.getSource()==button5){
             bhFrame.dispose();;
             new IssueBook();
         }
-
         if (e.getSource()==button6){
             bhFrame.dispose();
             new ReturnBook();
         }
-
         if (e.getSource()==button7){
             bhFrame.dispose();
             new ShowIssuedBooks();
