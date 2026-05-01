4631b79bb0784a3bd03f7a77dd054f0194163f1d
asrawazir <waziralamasra@gmail.com>
2026-05-01 19:35:41 +0500

    Creating instances for CreateFrame and CreateLabel class in MemberHomeFrame class

--- src/GUI/MemberFrames/MemberHomeFrame.java
+++ src/GUI/MemberFrames/MemberHomeFrame.java
@@ -1,5 +1,8 @@
 package GUI.MemberFrames;
 
+import GUI.AdditionalClasses.CreateFrame;
+import GUI.AdditionalClasses.CreateLabel;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,6 +10,8 @@ import javax.swing.*;
 
 public class MemberHomeFrame implements ActionListener {
 
+    CreateFrame mhFrame;
+    CreateLabel mhLabel;
     @Override
     public void actionPerformed(ActionEvent e) {
 
