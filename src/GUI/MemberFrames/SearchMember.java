0aa082f386f4df901201e85c47d3d70f206d5b2c
asrawazir <waziralamasra@gmail.com>
2026-05-01 20:39:00 +0500

    Implementing ActionListener Interface in the SearchMember class along with teh required imports and overriding its actionPerformed method

---
+++ src/GUI/MemberFrames/SearchMember.java
@@ -0,0 +1,11 @@
+package GUI.MemberFrames;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SearchMember implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
