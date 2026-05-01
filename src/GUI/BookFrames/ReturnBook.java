81540c78797fe5fce03f99c78203ea0877abd1c2
albashzia <ahmedalbashzia@gmail.com>
2026-05-01 05:32:40 +0500

    Creating JLabel and JTextField objects in ReturnBook  class

--- src/GUI/BookFrames/ReturnBook.java
+++ src/GUI/BookFrames/ReturnBook.java
@@ -5,11 +5,19 @@ import GUI.AdditionalClasses.CreateExitButton;
 import GUI.AdditionalClasses.CreateFrame;
 import GUI.AdditionalClasses.CreateLabel;
 
+import javax.swing.*;
+
 public class ReturnBook {
     CreateFrame rbFrame = new CreateFrame();
     CreateLabel label = new CreateLabel("Return Book");
     CreateExitButton exitButton = new CreateExitButton();
     CreateBackButton backButton = new CreateBackButton();
+
+    JLabel memberIdLabel = new JLabel();
+    JLabel bookIdLabel = new JLabel();
+
+    JTextField memberIdTextField = new JTextField();
+    JTextField bookIdTextField = new JTextField();
 
     public ReturnBook(){
 
