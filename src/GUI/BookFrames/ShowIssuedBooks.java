aaf972eb820df48e101bc76c60bb04e85670c876
albashzia <ahmedalbashzia@gmail.com>
2026-05-01 05:49:53 +0500

    Creating the objects of CreateBackButton and CreateExitButton class inside ShowIssuedBooks class and adding them to the frame

--- src/GUI/BookFrames/ShowIssuedBooks.java
+++ src/GUI/BookFrames/ShowIssuedBooks.java
@@ -1,13 +1,18 @@
 package GUI.BookFrames;
 
-import GUI.AdditionalClasses.CreateFrame;
-import GUI.AdditionalClasses.CreateLabel;
+import GUI.AdditionalClasses.*;
 
 public class ShowIssuedBooks {
     CreateFrame siFrame = new CreateFrame();
     CreateLabel label = new CreateLabel("Issued Books");
+
+    CreateBackButton backButton = new CreateBackButton();
+    CreateExitButton exitButton = new CreateExitButton();
+
     public ShowIssuedBooks(){
 
+        siFrame.add(backButton);
+        siFrame.add(exitButton);
         siFrame.add(label);
         siFrame.setVisible(true);
     }
