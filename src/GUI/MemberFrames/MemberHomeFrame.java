dd328a0831f9f9ba6be571b3b9ef4c43b2bf9064
asrawazir <waziralamasra@gmail.com>
2026-05-01 19:34:25 +0500

    Implementing ActionListener interface in the MemberHomeFrame class along with the required imports and overriding its action performed method

--- src/GUI/MemberFrames/MemberHomeFrame.java
+++ src/GUI/MemberFrames/MemberHomeFrame.java
@@ -1,5 +1,14 @@
 package GUI.MemberFrames;
 
-public class MemberHomeFrame {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
+public class MemberHomeFrame implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
