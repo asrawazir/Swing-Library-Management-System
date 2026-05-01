b51dbcb98f44291ef4cc6f3bb26e71c022521fc2
Eshal <eshalnaeem2008@gmail.com>
2026-05-01 18:04:50 +0500

    Implemented Action listener for DisplayAllRoomsFrames controls

--- src/GUI/StudyRoomFrames/DisplayAllRoomsFrame.java
+++ src/GUI/StudyRoomFrames/DisplayAllRoomsFrame.java
@@ -3,44 +3,52 @@ package GUI.StudyRoomFrames;
 import GUI.AdditionalClasses.*;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class DisplayAllRoomsFrame  {
+public class DisplayAllRoomsFrame implements ActionListener {
     CreateFrame frame;
     CreateLabel label;
     CreateBackButton backButton;
 
     CreateExitButton exitButton;
 
-   public  DisplayAllRoomsFrame(){
-       frame=new CreateFrame();
-       label=new CreateLabel("Display All Rooms");
-       backButton=new CreateBackButton();
-      exitButton=new CreateExitButton();
+    public DisplayAllRoomsFrame() {
+        frame = new CreateFrame();
+        label = new CreateLabel("Display All Rooms");
+        backButton = new CreateBackButton();
+        exitButton = new CreateExitButton();
 
-      String[] column={"Room Number","Status","Reserved By"};
+        backButton.addActionListener(this);
+        exitButton.addActionListener(this);
+        String[] column = {"Room Number", "Status", "Reserved By"};
 
-      String [][]data={
-              {"101","Available","null"},
-              {"102","Available","null"},
-              {"103","Available","null"},
-              {"104","Available","null"}
-      };
+        String[][] data = {
+                {"101", "Available", "null"},
+                {"102", "Available", "null"},
+                {"103", "Available", "null"},
+                {"104", "Available", "null"}
+        };
 
-      JTable table=new JTable(data,column);
-      JScrollPane scrollPane=new JScrollPane(table);
+        JTable table = new JTable(data, column);
+        JScrollPane scrollPane = new JScrollPane(table);
 
-      scrollPane.setBounds(100,150,500,90);
-      frame.add(label);
-      frame.add(backButton);
-      frame.add(exitButton);
+        scrollPane.setBounds(100, 150, 500, 90);
+        frame.add(label);
+        frame.add(backButton);
+        frame.add(exitButton);
 
-      frame.add(scrollPane);
+        frame.add(scrollPane);
 
-      frame.setVisible(true);
+        frame.setVisible(true);
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
-
-
+        if (e.getSource() == backButton) {
+            frame.dispose();
+            new StudyRoomHomeFrame();
+        }
     }
 }
