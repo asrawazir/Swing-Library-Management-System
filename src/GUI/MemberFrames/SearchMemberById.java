c475688ec960e653add2c7c7ff6a22c545062512
asrawazir <waziralamasra@gmail.com>
2026-05-02 23:17:36 +0500

    Creating global instances for CreateSubmitButton class in SearchMemberById class

--- src/GUI/MemberFrames/SearchMemberById.java
+++ src/GUI/MemberFrames/SearchMemberById.java
@@ -1,9 +1,20 @@
 package GUI.MemberFrames;
 
+import GUI.AdditionalClasses.CreateFrame;
+import GUI.AdditionalClasses.CreateLabel;
+import GUI.AdditionalClasses.CreateSubmitButton;
+
+import java.awt.*;
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class SearchMemberById implements ActionListener {
+
+    CreateFrame siFrame;
+    CreateLabel siLabel;
+    CreateSubmitButton siSubmitButton;
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
