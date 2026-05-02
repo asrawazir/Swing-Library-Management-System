35615b7b6c64026ce05be601ebe842e3ebb7ac38
albashzia <ahmedalbashzia@gmail.com>
2026-05-02 10:22:11 +0500

    Commenting the code in DisplayBooks class to improve the readability and understandability of the code

--- src/GUI/BookFrames/DisplayBooks.java
+++ src/GUI/BookFrames/DisplayBooks.java
@@ -9,23 +9,39 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class DisplayBooks implements ActionListener {
+
+    //Creating a basic frame
     CreateFrame dbFrame = new CreateFrame();
+
+    //Defining the main label
     CreateLabel label = new CreateLabel("Display Books");
+
+    //Creating buttons using pre-defined button classes
     CreateBackButton backButton = new CreateBackButton();
     CreateExitButton exitButton = new CreateExitButton();
 
+    //Default Constructor
     public DisplayBooks(){
 
+        //Adding action listener method to back button
         backButton.addActionListener(this);
 
+        //Adding main label to the frame
+        dbFrame.add(label);
+
+        //Adding buttons to the frame
         dbFrame.add(backButton);
         dbFrame.add(exitButton);
-        dbFrame.add(label);
+
+        //Setting the visibility of frame to true
         dbFrame.setVisible(true);
     }
 
+    //Overriding actionPerformed method
     @Override
     public void actionPerformed(ActionEvent e){
+
+        //Processing the action received by backButton
         if (e.getSource()==backButton){
             dbFrame.dispose();
             new BookHomeFrame();
