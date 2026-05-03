2e3f3f0c1be3d3a62a2f08d4feaaf2a061809a69
asrawazir <waziralamasra@gmail.com>
2026-05-04 00:01:49 +0500

    Implementing ActionListener Interface for RemoveMember class along with the required imports and overriding its actionPerformed method

--- src/GUI/MemberFrames/RemoveMember.java
+++ src/GUI/MemberFrames/RemoveMember.java
@@ -1,4 +1,13 @@
 package GUI.MemberFrames;
 
-public class RemoveMember {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class RemoveMember implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
