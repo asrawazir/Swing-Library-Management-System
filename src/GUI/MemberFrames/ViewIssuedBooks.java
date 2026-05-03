8f83d6930a62e799d1b092776b1b87979f2ed31f
asrawazir <waziralamasra@gmail.com>
2026-05-03 23:19:03 +0500

    Creating global instances of CreateFrame and CreateLabel class in ViewIssuedBooks class

--- src/GUI/MemberFrames/ViewIssuedBooks.java
+++ src/GUI/MemberFrames/ViewIssuedBooks.java
@@ -1,9 +1,17 @@
 package GUI.MemberFrames;
 
+import GUI.AdditionalClasses.CreateFrame;
+import GUI.AdditionalClasses.CreateLabel;
+
+import java.awt.*;
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ViewIssuedBooks implements ActionListener {
+
+    CreateFrame ibFrame;
+    CreateLabel ibLabel;
     @Override
     public void actionPerformed(ActionEvent e) {
 
