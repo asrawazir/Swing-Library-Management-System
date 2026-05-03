5a1eefa19e4f10f2b2d6c32e575e0ec0ccc2aedb
asrawazir <waziralamasra@gmail.com>
2026-05-04 00:02:58 +0500

    Creating global instances for CreateFarme and CreateLabel in RemoveMember class

--- src/GUI/MemberFrames/RemoveMember.java
+++ src/GUI/MemberFrames/RemoveMember.java
@@ -1,11 +1,17 @@
 package GUI.MemberFrames;
 
+import GUI.AdditionalClasses.CreateFrame;
+import GUI.AdditionalClasses.CreateLabel;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class RemoveMember implements ActionListener {
+
+    CreateFrame rmFrame;
+    CreateLabel rmLabel;
     @Override
     public void actionPerformed(ActionEvent e) {
 
