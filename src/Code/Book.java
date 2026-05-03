33d9504240951ec2d72691207e9d0ff608add1c8
albashzia <ahmedalbashzia@gmail.com>
2026-05-03 14:00:03 +0500

    Defining the default constructor for Book class

--- src/Code/Book.java
+++ src/Code/Book.java
@@ -6,4 +6,12 @@ public class Book {
     private String author;
     private int totalQuantity;
     private int availableQuantity;
+
+    public Book() {
+        this.id = 0;
+        this.title = "Not assigned";
+        this.author = "Unknown";
+        this.totalQuantity = 0;
+        this.availableQuantity = 0;
+    }
 }
