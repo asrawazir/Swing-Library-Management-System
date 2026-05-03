74cfca985404490bd8a43fabe553305616febf36
albashzia <ahmedalbashzia@gmail.com>
2026-05-03 13:59:30 +0500

    Defining the attributes of Book Class

--- src/Code/Book.java
+++ src/Code/Book.java
@@ -1,4 +1,9 @@
 package Code;
 
 public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private int totalQuantity;
+    private int availableQuantity;
 }
