65fccb10941329b6fd2f19fd591c75026a32281d
asrawazir <waziralamasra@gmail.com>
2026-05-03 22:57:53 +0500

    Creating instances for CreateFrame and CreateLabel class

--- src/GUI/MemberFrames/SearchMemberByName.java
+++ src/GUI/MemberFrames/SearchMemberByName.java
@@ -1,11 +1,17 @@
 package GUI.MemberFrames;
 
+import GUI.AdditionalClasses.CreateFrame;
+import GUI.AdditionalClasses.CreateLabel;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class SearchMemberByName implements ActionListener {
+    CreateFrame snFrame;
+    CreateLabel snLabel;
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
