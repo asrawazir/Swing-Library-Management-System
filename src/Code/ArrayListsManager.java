8fc9b1ba19db4be329e12807419de0c69f59ac9c
albashzia <ahmedalbashzia@gmail.com>
2026-05-04 22:08:34 +0500

    Defining the constructor of ArrayListsManager class

--- src/Code/ArrayListsManager.java
+++ src/Code/ArrayListsManager.java
@@ -5,7 +5,10 @@ import java.util.ArrayList;
 public class ArrayListsManager {
 
     public static ArrayListsManager instance = new ArrayListsManager();
-    ArrayList<Book> books = new ArrayList<>();
+    static ArrayList<Book> books;
+    public ArrayListsManager(){
+         books = new ArrayList<>();
+    }
 
     public void addBook(Book book){
         books.add(book);
