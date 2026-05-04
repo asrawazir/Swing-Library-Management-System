a09c675b97a3d76b7d9e7763ea2e278258ca5720
albashzia <ahmedalbashzia@gmail.com>
2026-05-04 12:43:11 +0500

    Defining an instance object of ArrayListsManager class in ArrayListsManager

--- src/Code/ArrayListsManager.java
+++ src/Code/ArrayListsManager.java
@@ -3,6 +3,8 @@ package Code;
 import java.util.ArrayList;
 
 public class ArrayListsManager {
+
+    public static ArrayListsManager instance = new ArrayListsManager();
     ArrayList<Book> books = new ArrayList<>();
 
     public void addBook(Book book){
