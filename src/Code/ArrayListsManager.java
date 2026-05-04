4237740e1aa84237df2886ce58ce93a9a4124b26
albashzia <ahmedalbashzia@gmail.com>
2026-05-04 12:28:31 +0500

    Creating addBook method in ArrayListsManager class to receive a Book object and adding it to the books array list

--- src/Code/ArrayListsManager.java
+++ src/Code/ArrayListsManager.java
@@ -5,4 +5,8 @@ import java.util.ArrayList;
 public class ArrayListsManager {
     ArrayList<Book> books = new ArrayList<>();
 
+    public void addBook(Book book){
+        books.add(book);
+    }
+
 }
