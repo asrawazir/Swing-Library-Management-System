40aceed7331a1593fa96b244d00b35d5342d2140
asrawazir <waziralamasra@gmail.com>
2026-05-05 10:10:24 +0500

    Initializing members arraylist in the default constructor of ArrayListsManager class

--- src/Code/ArrayListsManager.java
+++ src/Code/ArrayListsManager.java
@@ -7,8 +7,11 @@ public class ArrayListsManager {
     public static ArrayListsManager instance = new ArrayListsManager();
     static ArrayList<Book> books;
     static ArrayList<Member> members;
+
     public ArrayListsManager(){
-         books = new ArrayList<>();
+
+        books = new ArrayList<>();
+        members = new ArrayList<>();
     }
 
     public void addBook(Book book){
