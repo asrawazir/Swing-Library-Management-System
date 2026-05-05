2f7e730a849bd9373e3d347226d97cb4d009eaf3
Eshal <eshalnaeem2008@gmail.com>
2026-05-05 21:42:16 +0500

    Initialize system with pre-registered study rooms in Arraylist manager

--- src/Code/ArrayListsManager.java
+++ src/Code/ArrayListsManager.java
@@ -6,8 +6,23 @@ public class ArrayListsManager {
 
     public static ArrayListsManager instance = new ArrayListsManager();
     static ArrayList<Book> books;
-    public ArrayListsManager(){
-         books = new ArrayList<>();
+    ArrayList<StudyRoom> rooms;
+
+    public ArrayListsManager() {
+
+        books = new ArrayList<>();
+        rooms = new ArrayList<>();
+        rooms.add(new StudyRoom("101"));
+        rooms.add(new StudyRoom("102"));
+        rooms.add(new StudyRoom("103"));
+        rooms.add(new StudyRoom("104"));
+        rooms.add(new StudyRoom("105"));
+        rooms.add(new StudyRoom("106"));
+        rooms.add(new StudyRoom("107"));
+        rooms.add(new StudyRoom("108"));
+        rooms.add(new StudyRoom("109"));
+        rooms.add(new StudyRoom("110"));
+
     }
 
     public void addBook(Book book){
