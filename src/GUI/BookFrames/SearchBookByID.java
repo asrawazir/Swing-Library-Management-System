e670c1b63a6bf41028e4a0ee165c282b83beb11a
albashzia <ahmedalbashzia@gmail.com>
2026-05-05 11:18:19 +0500

    Adding body to actionPerformed method to get the Book id from idTextField

--- src/GUI/BookFrames/SearchBookByID.java
+++ src/GUI/BookFrames/SearchBookByID.java
@@ -47,5 +47,9 @@ public class SearchBookByID implements ActionListener {
             sbiFrame.dispose();
             new SearchBook();
         }
+
+        if (e.getSource()==submitButton){
+            String id = idTextField.getText();
+        }
     }
 }
