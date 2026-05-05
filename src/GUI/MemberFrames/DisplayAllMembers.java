50aacece78155406cc12942e4f77cfb4f9e4a06f
asrawazir <waziralamasra@gmail.com>
2026-05-05 10:30:47 +0500

    Implementing ActionListener interface in DisplayAllMembers class along with the necessary imports and overriding its actionPerformed method

--- src/GUI/MemberFrames/DisplayAllMembers.java
+++ src/GUI/MemberFrames/DisplayAllMembers.java
@@ -1,4 +1,13 @@
 package GUI.MemberFrames;
 
-public class DisplayAllMembers {
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DisplayAllMembers implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
