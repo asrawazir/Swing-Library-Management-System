ae5c1aeddd4082b102ecd0739b7caa2a12903e9e
asrawazir <waziralamasra@gmail.com>
2026-05-05 10:32:32 +0500

    Creating global instance for CreateFrame and CreateLabel class in the DisplayAllMembers class

--- src/GUI/MemberFrames/DisplayAllMembers.java
+++ src/GUI/MemberFrames/DisplayAllMembers.java
@@ -1,11 +1,18 @@
 package GUI.MemberFrames;
 
+import GUI.AdditionalClasses.CreateFrame;
+import GUI.AdditionalClasses.CreateLabel;
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class DisplayAllMembers implements ActionListener {
+
+    CreateFrame dmFrame;
+    CreateLabel dmLabel;
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
