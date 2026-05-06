043d81469f1f34acafbe417c5001eec67196ff66
asrawazir <waziralamasra@gmail.com>
2026-05-06 16:47:54 +0500

    Setting the bounds of the background label in the MainFrame class

--- src/GUI/MainFrame.java
+++ src/GUI/MainFrame.java
@@ -24,7 +24,7 @@ public class MainFrame implements ActionListener {
         ImageIcon backgroundIcon = new ImageIcon("img3.jpg");
         Image scaledImage = backgroundIcon.getImage().getScaledInstance(700, 500, Image.SCALE_SMOOTH);
         backgroundLabel = new JLabel(new ImageIcon(scaledImage));
-
+        backgroundLabel.setBounds(0, 0, 700, 500);
     }
 
 
