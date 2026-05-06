466b72926597f5cbb0a7aea5a139ed7775083bb6
asrawazir <waziralamasra@gmail.com>
2026-05-06 15:42:44 +0500

    Implementing ActionListener interface in MainFrame class along with the required imports and overriding its actionPerformed method

--- src/GUI/MainFrame.java
+++ src/GUI/MainFrame.java
@@ -1,12 +1,18 @@
 package GUI;
 
 import GUI.AdditionalClasses.CreateFrame;
+import GUI.AdditionalClasses.CreateSubmitButton;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class MainFrame extends JFrame{
+public class MainFrame implements ActionListener {
 
-    CreateFrame mFrame;
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
