f4f3c27cedb731aa0ac569d4c8014391033a18bc
albashzia <ahmedalbashzia@gmail.com>
2026-05-06 06:30:00 +0500

    Adding rooms ArrayList to ArrayListsManager class

--- src/Code/ArrayListsManager.java
+++ src/Code/ArrayListsManager.java
@@ -7,11 +7,22 @@ public class ArrayListsManager {
     public static ArrayListsManager instance = new ArrayListsManager();
     static ArrayList<Book> books;
     ArrayList<Member> members;
+    ArrayList<StudyRoom> rooms;
 
     public ArrayListsManager(){
 
         books = new ArrayList<>();
         members = new ArrayList<>();
+        rooms.add(new StudyRoom("101"));
+        rooms.add(new StudyRoom("102"));
+        rooms.add(new StudyRoom("103"));
+        rooms.add(new StudyRoom("104"));
+        rooms.add(new StudyRoom("105"));
+        rooms.add(new StudyRoom("106"));
+        rooms.add(new StudyRoom("107"));
+        rooms.add(new StudyRoom("108"));
+        rooms.add(new StudyRoom("109"));
+        rooms.add(new StudyRoom("110"));
     }
 
     public void addBook(Book book){
