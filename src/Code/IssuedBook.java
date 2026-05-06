7005f44dd5daa4169b0a6a907aabde50b92c524a
albashzia <ahmedalbashzia@gmail.com>
2026-05-06 05:27:00 +0500

    Defining the data member attributes of IssuedBook class

--- src/Code/IssuedBook.java
+++ src/Code/IssuedBook.java
@@ -1,4 +1,8 @@
 package Code;
 
 public class IssuedBook {
+    private Book book; // associated with Book object
+    private Member member; //associated with Member object
+    private String issueDate;
+
 }
