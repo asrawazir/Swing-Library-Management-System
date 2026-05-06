8dc848c04298fd992c0be8d66fc532aabcae76b9
asrawazir <waziralamasra@gmail.com>
2026-05-06 15:38:26 +0500

    Adding changes to CreateFrame class

--- src/GUI/AdditionalClasses/CreateFrame.java
+++ src/GUI/AdditionalClasses/CreateFrame.java
@@ -5,7 +5,6 @@ import javax.swing.*;
 
 public class CreateFrame extends JFrame{
 
-    JLabel backgroundLabel;
     //default Constructor
     public CreateFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -15,14 +14,6 @@ public class CreateFrame extends JFrame{
         this.setLocationRelativeTo(null);
         this.setLayout(null);
         this.setTitle("Library Management System");
-
-        String path = "C:\\Users\\FA25-BSE-017.CUI\\Pictures\\img\\img3.jpg";
-        ImageIcon backgroundIcon = new ImageIcon(path);
-        Image scaledImage = backgroundIcon.getImage().getScaledInstance(700, 500, Image.SCALE_SMOOTH);
-        backgroundLabel = new JLabel(new ImageIcon(scaledImage));
-        backgroundLabel.setBounds(0, 0, 700, 500);
-
-        this.add(backgroundLabel);
     }
 
 }
