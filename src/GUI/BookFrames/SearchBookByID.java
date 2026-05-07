d4e355314bd0c69a03a792af076c2936a0b8deef
albashzia <ahmedalbashzia@gmail.com>
2026-05-07 10:07:00 +0500

    Modifying the bounds of idLabel and idTextField for SearchBookByID frame

--- src/GUI/BookFrames/SearchBookByID.java
+++ src/GUI/BookFrames/SearchBookByID.java
@@ -24,11 +24,11 @@ public class SearchBookByID implements ActionListener {
     public SearchBookByID(){
 
         idLabel.setText("Enter Book ID : ");
-        idLabel.setBounds(10,120,680,30);
+        idLabel.setBounds(10,120,500,30);
         idLabel.setFont(new Font("Inter",Font.BOLD,14));
         idLabel.setBackground(Color.LIGHT_GRAY);
 
-        idTextField.setBounds(10,160,670,30);
+        idTextField.setBounds(10,160,500,30);
         idTextField.setFont(new Font("Inter",Font.PLAIN,14));
         idTextField.setForeground(Color.BLACK);
         idTextField.setBackground(Color.WHITE);
