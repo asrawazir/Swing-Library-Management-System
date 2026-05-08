24e4e3aed685636a5c990fc5c4894d99e8490e99
Eshal <eshalnaeem2008@gmail.com>
2026-05-08 17:03:02 +0500

    update text color to white for readability

--- src/GUI/StudyRoomFrames/CheckReservationFrame.java
+++ src/GUI/StudyRoomFrames/CheckReservationFrame.java
@@ -46,6 +46,7 @@ public class CheckReservationFrame implements ActionListener {
          textField.setBounds(10,190,670,30);
          textField.setFont(new Font("Inter", Font.PLAIN,14));
          textField.setBackground(new Color(10,20,35));
+        textField.setForeground(Color.white);
         
 
         frame.add(label);
