ece0e538324c3653489a9b1a296d05e97c8de268
Eshal <eshalnaeem2008@gmail.com>
2026-05-08 15:30:03 +0500

    Changing the foreground and background color of submit button

--- src/GUI/AdditionalClasses/CreateSubmitButton.java
+++ src/GUI/AdditionalClasses/CreateSubmitButton.java
@@ -10,8 +10,8 @@ public class CreateSubmitButton extends JButton{
         this.setLocation(300,340);
         this.setSize(100,40);
         this.setFont(new Font("Roboto", Font.BOLD, 12));
-        this.setBackground(new Color(0,212,255));
-        this.setForeground(Color.BLACK);
+        this.setBackground(new Color(0, 212, 255));
+        this.setForeground(Color.black);
         this.setText(title);
         this.setOpaque(true);
         this.setFocusable(false);
