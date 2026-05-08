3f26b5b1b3e4f8f9cc3cc11c818f594ea23ff389
Eshal <eshalnaeem2008@gmail.com>
2026-05-08 21:36:18 +0500

    Adjusted text fields height for better alignment

--- src/GUI/StudyRoomFrames/CheckReservationFrame.java
+++ src/GUI/StudyRoomFrames/CheckReservationFrame.java
@@ -43,7 +43,7 @@ public class CheckReservationFrame implements ActionListener {
          jLabel.setForeground(Color.white);
 
 
-         textField.setBounds(10,190,670,30);
+         textField.setBounds(10,190,670,25);
          textField.setFont(new Font("Inter", Font.PLAIN,14));
          textField.setBackground(new Color(10,20,35));
         textField.setForeground(Color.white);
