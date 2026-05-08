e2dadccff6d14ec837ef2d304f1cc9e8e9b77488
Eshal <eshalnaeem2008@gmail.com>
2026-05-08 12:53:42 +0500

    Updated submit buttons background colors

--- src/GUI/AdditionalClasses/CreateSubmitButton.java
+++ src/GUI/AdditionalClasses/CreateSubmitButton.java
@@ -22,7 +22,7 @@ public class CreateSubmitButton extends JButton{
         this.setLocation(x,y);
         this.setSize(100,40);
         this.setFont(new Font("Roboto", Font.BOLD, 12));
-        this.setBackground(Color.LIGHT_GRAY);
+        this.setBackground(new Color(0,212,255));
         this.setForeground(Color.BLACK);
         this.setText(title);
         this.setOpaque(true);
