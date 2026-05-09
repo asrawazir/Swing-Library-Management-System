f430041604a1da0c02804510cb58c65fad38e725
albashzia <ahmedalbashzia@gmail.com>
2026-05-09 21:16:37 +0500

    Creating an object of FileManager class and calling the loadDataFromFile method

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import GUI.HomeFrame;
 import GUI.MainFrame;
+import Code.FileManager;
 
 public class Main
 {
+    static FileManager fileManager = new FileManager();
     public static void main(String[] args){
         //calling home frame class from main
+        fileManager.loadDataFromFile();
         new MainFrame();
     }
 }
