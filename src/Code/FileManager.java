0e7169032462b76b86e5d1ecab169002b281b08b
albashzia <ahmedalbashzia@gmail.com>
2026-05-09 21:16:11 +0500

    Adding body to loadDataFromFile method to load Books data from "Books.txt" file and adding it to the array list using instance object of ArrayListsManager class

--- src/Code/FileManager.java
+++ src/Code/FileManager.java
@@ -1,9 +1,26 @@
 package Code;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class FileManager {
+    ArrayListsManager alm =ArrayListsManager.instance;
     final String BOOKS_DATA_FILE = "Books.txt";
 
     public void loadDataFromFile(){
-
+        try {
+            File booksFile = new File(BOOKS_DATA_FILE);
+            if (booksFile.exists()){
+                Scanner sc = new Scanner(booksFile);
+                while (sc.hasNextLine()){
+                    String bookData = sc.nextLine();
+                    String[] parts = bookData.split(",");
+                    Book b = new Book(parts[0],parts[1],parts[2],parts[3]);
+                    alm.addBook(b);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
