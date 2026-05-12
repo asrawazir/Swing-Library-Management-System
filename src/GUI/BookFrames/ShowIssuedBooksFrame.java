dba86b00e3bad8eba219904947fd0891f35a0534
albashzia <ahmedalbashzia@gmail.com>
2026-05-12 17:00:35 +0500

    Defining the properties for JTable such as font color, font background,columns width and row height etc

--- src/GUI/BookFrames/ShowIssuedBooksFrame.java
+++ src/GUI/BookFrames/ShowIssuedBooksFrame.java
@@ -36,6 +36,18 @@ public class ShowIssuedBooksFrame implements ActionListener {
         JTable table = new JTable(data,headers);
         table.setBackground(new Color(10,20,35));
         table.setForeground(Color.white);
+        table.setSelectionBackground(new Color(0,120,215));
+        table.setSelectionForeground(Color.white);
+        table.setGridColor(new Color(40,60,90));
+        table.setRowHeight(20);
+        table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        table.getColumnModel().getColumn(0).setPreferredWidth(100);
+        table.getColumnModel().getColumn(1).setPreferredWidth(100);
+        table.getColumnModel().getColumn(2).setPreferredWidth(210);
+        table.getColumnModel().getColumn(3).setPreferredWidth(150);
+        table.getTableHeader().setBackground(new Color(10,15,35));
+        table.getTableHeader().setForeground(Color.CYAN);
+        table.getTableHeader().setFont(new Font("Arial",Font.BOLD,14));
 
         JScrollPane scrollPane = new JScrollPane(table);
         scrollPane.setBounds(70,120,560,250);
