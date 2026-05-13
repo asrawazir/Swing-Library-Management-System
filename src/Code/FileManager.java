07fe95d42a20e9a0eaad554c60f8b79617fe08a7
asrawazir <waziralamasra@gmail.com>
2026-05-14 01:54:15 +0500

    Changing the name of the method to saveMembersToFile in FileManager class

--- src/Code/FileManager.java
+++ src/Code/FileManager.java
@@ -43,7 +43,7 @@ public class FileManager {
 
     }
 
-    public void loadMembersFromFile(){
+    public void saveMembersToFile(){
         try{
             ObjectOutputStream mos = new ObjectOutputStream(new FileOutputStream(MEMBERS_DATA_FILE));
 
@@ -59,5 +59,9 @@ public class FileManager {
         {
             ex.printStackTrace();
         }
+    }
+
+    public void LoadMembersFromFile(){
+
     }
 }
