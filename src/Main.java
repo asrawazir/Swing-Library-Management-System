c76e5129cdf9c2fb46e8c0fff7bf0b1d9fb981a3
asrawazir <waziralamasra@gmail.com>
2026-05-16 00:13:30 +0500

    Loading members data from the file in the main method of the main class

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,10 @@ import Code.FileManager;
 public class Main
 {
     static FileManager fileManager = new FileManager();
-    fileManager.loadMembersFromFile();
     public static void main(String[] args){
         //calling home frame class from main
         fileManager.loadDataFromFile();
+        fileManager.loadMembersFromFile();
         new MainFrame();
     }
 }
